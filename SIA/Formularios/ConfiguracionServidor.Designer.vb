3620afab6b36d14be214bf36eecbf65b1111a5e3
adler <adler@DESKTOP-SGK80U9>
2019-08-25 00:01:10 -0500

    25-08-2019

--- SIA/Formularios/ConfiguracionServidor.Designer.vb
+++ SIA/Formularios/ConfiguracionServidor.Designer.vb
@@ -22,78 +22,197 @@ Partial Class ConfiguracionServidor
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ConfiguracionServidor))
-        Me.BunifuFlatButton1 = New Bunifu.Framework.UI.BunifuFlatButton()
-        Me.BunifuMaterialTextbox1 = New Bunifu.Framework.UI.BunifuMaterialTextbox()
+        Me.fd = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.BunifuMetroTextbox1 = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.BunifuMetroTextbox2 = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.BunifuMetroTextbox3 = New Bunifu.Framework.UI.BunifuMetroTextbox()
+        Me.txtIpServidor = New System.Windows.Forms.TextBox()
+        Me.txtBaseDatos = New System.Windows.Forms.TextBox()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.txtPass = New System.Windows.Forms.TextBox()
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.btnCancelar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'BunifuFlatButton1
+        'fd
         '
-        Me.BunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(87, Byte), Integer))
-        Me.BunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(87, Byte), Integer))
-        Me.BunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.BunifuFlatButton1.BorderRadius = 0
-        Me.BunifuFlatButton1.ButtonText = "BunifuFlatButton1"
-        Me.BunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.BunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray
-        Me.BunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent
-        Me.BunifuFlatButton1.Iconimage = CType(resources.GetObject("BunifuFlatButton1.Iconimage"), System.Drawing.Image)
-        Me.BunifuFlatButton1.Iconimage_right = Nothing
-        Me.BunifuFlatButton1.Iconimage_right_Selected = Nothing
-        Me.BunifuFlatButton1.Iconimage_Selected = Nothing
-        Me.BunifuFlatButton1.IconMarginLeft = 0
-        Me.BunifuFlatButton1.IconMarginRight = 0
-        Me.BunifuFlatButton1.IconRightVisible = True
-        Me.BunifuFlatButton1.IconRightZoom = 0R
-        Me.BunifuFlatButton1.IconVisible = True
-        Me.BunifuFlatButton1.IconZoom = 90.0R
-        Me.BunifuFlatButton1.IsTab = False
-        Me.BunifuFlatButton1.Location = New System.Drawing.Point(212, 224)
-        Me.BunifuFlatButton1.Name = "BunifuFlatButton1"
-        Me.BunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(139, Byte), Integer), CType(CType(87, Byte), Integer))
-        Me.BunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(CType(CType(36, Byte), Integer), CType(CType(129, Byte), Integer), CType(CType(77, Byte), Integer))
-        Me.BunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White
-        Me.BunifuFlatButton1.selected = False
-        Me.BunifuFlatButton1.Size = New System.Drawing.Size(241, 48)
-        Me.BunifuFlatButton1.TabIndex = 1
-        Me.BunifuFlatButton1.Text = "BunifuFlatButton1"
-        Me.BunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        Me.BunifuFlatButton1.Textcolor = System.Drawing.Color.White
-        Me.BunifuFlatButton1.TextFont = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.fd.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.fd.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.fd.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.fd.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.fd.BorderThickness = 3
+        Me.fd.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.fd.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.fd.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.fd.isPassword = False
+        Me.fd.Location = New System.Drawing.Point(60, 35)
+        Me.fd.Margin = New System.Windows.Forms.Padding(4)
+        Me.fd.Name = "fd"
+        Me.fd.Size = New System.Drawing.Size(129, 44)
+        Me.fd.TabIndex = 3
+        Me.fd.Text = "IP/Servidor :"
+        Me.fd.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
         '
-        'BunifuMaterialTextbox1
+        'BunifuMetroTextbox1
         '
-        Me.BunifuMaterialTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam
-        Me.BunifuMaterialTextbox1.Font = New System.Drawing.Font("Century Gothic", 9.75!)
-        Me.BunifuMaterialTextbox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.BunifuMaterialTextbox1.HintForeColor = System.Drawing.Color.Empty
-        Me.BunifuMaterialTextbox1.HintText = ""
-        Me.BunifuMaterialTextbox1.isPassword = False
-        Me.BunifuMaterialTextbox1.LineFocusedColor = System.Drawing.Color.Blue
-        Me.BunifuMaterialTextbox1.LineIdleColor = System.Drawing.Color.Gray
-        Me.BunifuMaterialTextbox1.LineMouseHoverColor = System.Drawing.Color.Blue
-        Me.BunifuMaterialTextbox1.LineThickness = 3
-        Me.BunifuMaterialTextbox1.Location = New System.Drawing.Point(231, 54)
-        Me.BunifuMaterialTextbox1.Margin = New System.Windows.Forms.Padding(4)
-        Me.BunifuMaterialTextbox1.Name = "BunifuMaterialTextbox1"
-        Me.BunifuMaterialTextbox1.Size = New System.Drawing.Size(253, 44)
-        Me.BunifuMaterialTextbox1.TabIndex = 2
-        Me.BunifuMaterialTextbox1.Text = "BunifuMaterialTextbox1"
-        Me.BunifuMaterialTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.BunifuMetroTextbox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox1.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox1.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox1.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox1.BorderThickness = 3
+        Me.BunifuMetroTextbox1.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.BunifuMetroTextbox1.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.BunifuMetroTextbox1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BunifuMetroTextbox1.isPassword = False
+        Me.BunifuMetroTextbox1.Location = New System.Drawing.Point(60, 87)
+        Me.BunifuMetroTextbox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.BunifuMetroTextbox1.Name = "BunifuMetroTextbox1"
+        Me.BunifuMetroTextbox1.Size = New System.Drawing.Size(129, 44)
+        Me.BunifuMetroTextbox1.TabIndex = 4
+        Me.BunifuMetroTextbox1.Text = "Base de Datos  :"
+        Me.BunifuMetroTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BunifuMetroTextbox2
+        '
+        Me.BunifuMetroTextbox2.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox2.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox2.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox2.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox2.BorderThickness = 3
+        Me.BunifuMetroTextbox2.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.BunifuMetroTextbox2.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.BunifuMetroTextbox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BunifuMetroTextbox2.isPassword = False
+        Me.BunifuMetroTextbox2.Location = New System.Drawing.Point(60, 139)
+        Me.BunifuMetroTextbox2.Margin = New System.Windows.Forms.Padding(4)
+        Me.BunifuMetroTextbox2.Name = "BunifuMetroTextbox2"
+        Me.BunifuMetroTextbox2.Size = New System.Drawing.Size(129, 44)
+        Me.BunifuMetroTextbox2.TabIndex = 5
+        Me.BunifuMetroTextbox2.Text = "Usuario :"
+        Me.BunifuMetroTextbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BunifuMetroTextbox3
+        '
+        Me.BunifuMetroTextbox3.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox3.BorderColorFocused = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox3.BorderColorIdle = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox3.BorderColorMouseHover = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BunifuMetroTextbox3.BorderThickness = 3
+        Me.BunifuMetroTextbox3.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.BunifuMetroTextbox3.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.BunifuMetroTextbox3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BunifuMetroTextbox3.isPassword = False
+        Me.BunifuMetroTextbox3.Location = New System.Drawing.Point(60, 191)
+        Me.BunifuMetroTextbox3.Margin = New System.Windows.Forms.Padding(4)
+        Me.BunifuMetroTextbox3.Name = "BunifuMetroTextbox3"
+        Me.BunifuMetroTextbox3.Size = New System.Drawing.Size(129, 44)
+        Me.BunifuMetroTextbox3.TabIndex = 6
+        Me.BunifuMetroTextbox3.Text = "Contraseña :"
+        Me.BunifuMetroTextbox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'txtIpServidor
+        '
+        Me.txtIpServidor.AccessibleName = "txtIpServidor"
+        Me.txtIpServidor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtIpServidor.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtIpServidor.Location = New System.Drawing.Point(197, 42)
+        Me.txtIpServidor.Multiline = True
+        Me.txtIpServidor.Name = "txtIpServidor"
+        Me.txtIpServidor.Size = New System.Drawing.Size(253, 28)
+        Me.txtIpServidor.TabIndex = 11
+        '
+        'txtBaseDatos
+        '
+        Me.txtBaseDatos.AccessibleName = "txtBaseDatos"
+        Me.txtBaseDatos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtBaseDatos.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBaseDatos.Location = New System.Drawing.Point(197, 94)
+        Me.txtBaseDatos.Multiline = True
+        Me.txtBaseDatos.Name = "txtBaseDatos"
+        Me.txtBaseDatos.Size = New System.Drawing.Size(253, 28)
+        Me.txtBaseDatos.TabIndex = 12
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.AccessibleName = "txtUsuario"
+        Me.txtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtUsuario.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtUsuario.Location = New System.Drawing.Point(197, 146)
+        Me.txtUsuario.Multiline = True
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(253, 28)
+        Me.txtUsuario.TabIndex = 13
+        '
+        'txtPass
+        '
+        Me.txtPass.AccessibleName = "txtContraseña"
+        Me.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPass.Location = New System.Drawing.Point(197, 198)
+        Me.txtPass.Multiline = True
+        Me.txtPass.Name = "txtPass"
+        Me.txtPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtPass.Size = New System.Drawing.Size(253, 28)
+        Me.txtPass.TabIndex = 14
+        '
+        'btnConectar
+        '
+        Me.btnConectar.BackColor = System.Drawing.Color.FromArgb(CType(CType(10, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnConectar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConectar.Location = New System.Drawing.Point(499, 78)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(143, 44)
+        Me.btnConectar.TabIndex = 15
+        Me.btnConectar.Text = "Conectar"
+        Me.btnConectar.UseVisualStyleBackColor = False
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.BackColor = System.Drawing.Color.Red
+        Me.btnCancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCancelar.Location = New System.Drawing.Point(499, 134)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(143, 44)
+        Me.btnCancelar.TabIndex = 16
+        Me.btnCancelar.Text = "Cancelar"
+        Me.btnCancelar.UseVisualStyleBackColor = False
         '
         'ConfiguracionServidor
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(699, 300)
-        Me.Controls.Add(Me.BunifuMaterialTextbox1)
-        Me.Controls.Add(Me.BunifuFlatButton1)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(75, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(699, 275)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Controls.Add(Me.txtPass)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.txtBaseDatos)
+        Me.Controls.Add(Me.txtIpServidor)
+        Me.Controls.Add(Me.BunifuMetroTextbox3)
+        Me.Controls.Add(Me.BunifuMetroTextbox2)
+        Me.Controls.Add(Me.BunifuMetroTextbox1)
+        Me.Controls.Add(Me.fd)
+        Me.Cursor = System.Windows.Forms.Cursors.No
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Name = "ConfiguracionServidor"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Configuracion de Servidor"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents BunifuFlatButton1 As Bunifu.Framework.UI.BunifuFlatButton
-    Friend WithEvents BunifuMaterialTextbox1 As Bunifu.Framework.UI.BunifuMaterialTextbox
+    Friend WithEvents fd As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents BunifuMetroTextbox1 As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents BunifuMetroTextbox2 As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents BunifuMetroTextbox3 As Bunifu.Framework.UI.BunifuMetroTextbox
+    Friend WithEvents txtIpServidor As TextBox
+    Friend WithEvents txtBaseDatos As TextBox
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents txtPass As TextBox
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents btnCancelar As Button
 End Class
--- SIA/Formularios/ConfiguracionServidor.vb
+++ SIA/Formularios/ConfiguracionServidor.vb
@@ -1,3 +1,12 @@
 ﻿Public Class ConfiguracionServidor
+    Private Sub btnConectar_Click_1(sender As Object, e As EventArgs) Handles btnConectar.Click
+        If txtIpServidor.Text = "" Or txtPass.Text = "" Or txtBaseDatos.Text = "" Or txtUsuario.Text = "" Then
+            MsgBox("Aviso", vbCritical, "Operacion cancelada")
+        End If
+    End Sub
 
+    Private Sub btnCancelar_Click_1(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        Me.Close()
+        Me.Dispose()
+    End Sub
 End Class
